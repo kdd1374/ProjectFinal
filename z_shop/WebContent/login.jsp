e73b1a77ee76f1254212b407b78f969bbc3e36cd
kimjoong <wndgus4444@gmail.com>
2019-02-27 15:32:23 +0900

    아이디찾기

--- z_shop/WebContent/login.jsp
+++ z_shop/WebContent/login.jsp
@@ -1,15 +1,15 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<%
-String mesg=(String)request.getAttribute("mesg");
-%>
 <!DOCTYPE html>
 <html>
 <head>
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script type="text/javascript">
+<%
+String mesg=(String)request.getAttribute("loginmesg");
+%>
 <%if(mesg!=null){%>
-alert("<%=mesg%>")
+alert("<%=mesg%>");
 <%}%>
 </script>
 <link rel="stylesheet" type="text/css" href="common/main.css" />
--- z_shop/src/com/controller/member/IdSearchServlet.java
+++ z_shop/src/com/controller/member/IdSearchServlet.java
@@ -18,28 +18,8 @@ import com.service.MemberService;
 public class IdSearchServlet extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session = request.getSession();
-		String username=request.getParameter("username");
-		String email = request.getParameter("email");
-		String [] e = email.split("@");
-		String email1= e[0];
-		String email2= e[1];
-		HashMap<String, String> map = new HashMap<>();
-		map.put("username", username);
-		map.put("email1", email1);
-		map.put("email2", email2);
-		String page = "";
-		MemberService ser = new MemberService();
-		MemberDTO dto = ser.idSearch(map);
-		if(dto == null) {
-			String mesg = "존재하지 않는 회원정보입니다.";
-			session.setAttribute("searchmesg", mesg);
-			page = "idsearch.jsp";
-		}else {
-			session.setAttribute("findId", dto.getUserid());
-			page = "FindIdServlet";
-		}
-		response.sendRedirect(page);
+		
+		response.sendRedirect("idsearch.jsp");
 		
 		
 	}
